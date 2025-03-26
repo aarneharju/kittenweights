869e29a4e12a1b7d4fe3b212e0e8a0327afaaa54
aarneharju <35223549+aarneharju@users.noreply.github.com>
2025-03-26 08:44:48 +0200

    Added measurement 26.3.2025 Aamu

--- Gaia/2025-C-litter/kittenWeights.js
+++ Gaia/2025-C-litter/kittenWeights.js
@@ -1,7 +1,7 @@
-let labelArray = ['22.3.2025 Aamu', '22.3.2025 Ilta', '23.3.2025 Aamu', '23.3.2025 Ilta', '24.3.2025 Aamu', '24.3.2025 Ilta', '25.3.2025 Aamu - pantojen vaihto', '25.3.2025 Ilta'];
-let greenWeights = [104, 116, 122, 131, 138, 147, 157, 167];
-let pinkWeights = [77, 87, 90, 98, 105, 120, 127, 137];
-let nakedWeights = [82, 94, 100, 103, 116, 125, 130, 141];
+let labelArray = ['22.3.2025 Aamu', '22.3.2025 Ilta', '23.3.2025 Aamu', '23.3.2025 Ilta', '24.3.2025 Aamu', '24.3.2025 Ilta', '25.3.2025 Aamu - pantojen vaihto', '25.3.2025 Ilta', '26.3.2025 Aamu'];
+let greenWeights = [104, 116, 122, 131, 138, 147, 157, 167, 179];
+let pinkWeights = [77, 87, 90, 98, 105, 120, 127, 137, 144];
+let nakedWeights = [82, 94, 100, 103, 116, 125, 130, 141, 147];
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'line',
